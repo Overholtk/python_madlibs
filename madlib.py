5d6c79f2b605a377706febb45b972bf9be14f97f
KAYA OVERHOLT <OVERHOLTKAYA@GMAIL.COM>
2023-09-05 16:57:55 -0700

    initial commit

---
+++ madlib.py
@@ -0,0 +1,17 @@
+
+
+greeting = input('give me a greeting: ').capitalize()
+color1 = input('give me a color: ').capitalize()
+illness = input('give me an illness: ').capitalize()
+animal = input('give me an animal: ').capitalize()
+noun = input('give me a noun: ').lower()
+color2 = input('give me a color: ').lower()
+color3 = input('give me a color again: ').lower()
+body = input('give me a part of the body: ').lower()
+adj = input('give me an adjective: ').lower()
+color4 = input('give me one more color: ').lower()
+plurNoun = input('give me a plural noun: ').lower()
+propNoun = input('give me a proper noun: ').capitalize()
+explative = input('give me an explative!: ').lower()
+
+print(greeting + ' my namne is ' + color1 + ' Dark\'ness ' + illness + ' ' + animal + ' Way and I have long ebony black hair (that\'s how I got my ' + noun + ') with ' + color2 + ' streaks and ' + color3 + ' tips that reaches my ' + body + ' and ' + adj + ' ' + color4 + ' eyes like limpid ' + plurNoun + ' and a lot of people tell me I look like ' + propNoun + ' (AN: if u don\'t know who she is get da ' + explative + ' out of here!)')
